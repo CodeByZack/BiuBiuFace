582710eb9ea10662742f7efffb1786ebfa3a4c41
zackdk <zackvvzackvv@gmail.com>
2020-07-20 18:02:52 +0800

    gif 解析库

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,24 +1,43 @@
-import React, { Component } from 'react'
-import { View, Text } from '@tarojs/components'
-import './index.scss'
+import React, { Component, useEffect,useRef } from "react";
+import { View, Text, Canvas } from "@tarojs/components";
+import Taro from '@tarojs/taro';
+import { parseGIF, decompressFrames } from '../../lib/gif';
+import "./index.scss";
 
-export default class Index extends Component {
+const Index = () => {
+  const canvasRef = useRef();
+  useEffect(() => {
 
-  componentWillMount () { }
+    const getGif = async ()=>{
 
-  componentDidMount () { }
+      const res = await Taro.request({url: "http://127.0.0.1:5500/demo/horses.gif",responseType:"arraybuffer"})
+      console.log(res);
+      const gif = parseGIF(res.data)
+      console.log(gif);
+      var frames = decompressFrames(gif, true)
+      // render the gif
+      console.log(frames);
+      console.log(canvasRef);
+      const canvas = canvasRef.current;
+      console.log(canvas);
+      const ctx = canvas.getContext('2d')
+      canvas.width = res[0].width
+      canvas.height = res[0].height
 
-  componentWillUnmount () { }
+      ctx.fillRect(0, 0, 100, 100)
 
-  componentDidShow () { }
 
-  componentDidHide () { }
+    };
+    getGif();
 
-  render () {
-    return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-      </View>
-    )
-  }
-}
+  }, []);
+
+  return (
+    <View className="index">
+      <Text>Hello world!</Text>
+      <Canvas ref={canvasRef} type="2d" id="canvas" />
+    </View>
+  );
+};
+
+export default Index;
